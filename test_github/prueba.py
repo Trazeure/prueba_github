a916696d34d23bc14e72225995dece7f131d9493
Trazeure <Destiny72002@outlook.com>
2023-09-27 23:34:55 -0600

    Se agrego setup.py y prueba.py

---
+++ test_github/prueba.py
@@ -0,0 +1,29 @@
+import math
+import random
+import string
+
+def primo(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def siguiente_primo(n):
+    candidate = n + 1
+    while not primo(candidate):
+        candidate += 1
+    return candidate
+
+def mediana(a, b, c):
+    return sorted([a, b, c])[1]
+
+def password():
+    length = random.randint(7, 10)
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+def hipotenusa(a, b):
+    return math.sqrt(a**2 + b**2)
---
+++ test_github/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='prueba_github',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/Trazeure/prueba_github.git',
+    author='Trazeure',
+    author_email='trazeure@hotmail.com'
+)
